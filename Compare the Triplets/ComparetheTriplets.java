a6882ce66d4a53297e9c738259e8ebe60240f26c
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-08-27 15:09:22 +0200

    added new file

---
+++ Compare the Triplets/ComparetheTriplets.java
@@ -0,0 +1,71 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ComparetheTriplets {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> abc=new ArrayList<>();
+        int num1=0;int num2=0;
+         for (int i = 0; i < 3; i++) {
+            int aItem = a.get(i);
+            int bItem = b.get(i);
+            if(aItem>bItem){
+                num1+=1;
+            }
+            else if(bItem>aItem){
+                num2+=1;
+            }
+            
+        }
+        abc.add(num1);
+        abc.add(num2);
+        return abc;
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] aItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a.add(aItem);
+        }
+
+        String[] bItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bItems[i]);
+            b.add(bItem);
+        }
+
+        List<Integer> result = compareTriplets(a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
