d559c25b839e3ca18bb61dba6ca1d8408bcf6cb3
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-08-28 12:42:39 +0200

    Update BeautifulBinaryString.java

--- BeautifulBinaryString/BeautifulBinaryString.java
+++ BeautifulBinaryString/BeautifulBinaryString.java
@@ -1,35 +1,53 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-public class BeautifulBinaryString {
+public class Solution {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+    // Complete the beautifulBinaryString function below.
+    static int beautifulBinaryString(String b) {
 
-		String b="000001111111101010101";
-		int cnt=0;
-		for(int i=0;i<b.length();)
-		{
-			if(i<b.length()-2)
-			{
-				if(b.charAt(i) =='0' && b.charAt(i+1)=='1' && b.charAt(i+2)=='0')
-				{
-					cnt++;
-					i=i+3;
-				}
-				else i++;
-			}
-			else i++;
-			
-		}
-		System.out.println(cnt);
-       
-      
-	
-	}
+        int cnt=0;
+        for(int i=0;i<b.length();)
+        {
+            if(i<b.length()-2)
+            {
+                if(b.charAt(i) =='0' && b.charAt(i+1)=='1' && b.charAt(i+2)=='0')
+                {
+                    cnt++;
+                    i=i+3;
+                }
+                else i++;
+            }
+            else i++;
+            
+        }
+        return cnt;
 
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String b = scanner.nextLine();
+
+        int result = beautifulBinaryString(b);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
