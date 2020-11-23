db0710a883217125dc0df77724eca30e04c9fc67
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-11-23 04:50:07 +0100

    Problem solution is accepted in hackerrank

---
+++ Connecting Towns/connectingTowns.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the connectingTowns function below.
+#
+def connectingTowns(n, routes):
+    #
+    # Write your code here.
+    #
+    t=1
+    for str in range(n-1):
+        t=t*routes[str]
+    return t%1234567
+
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        routes = list(map(int, input().rstrip().split()))
+
+        result = connectingTowns(n, routes)
+        print(result)
+       # fptr.write(str(result) + '\n')
+
+   # fptr.close()
