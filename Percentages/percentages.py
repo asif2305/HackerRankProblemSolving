cee011e82da6998d0a30907c0910cf50d0c5546f
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-11-25 23:10:40 +0100

    percentage problems

---
+++ Percentages/percentages.py
@@ -0,0 +1,17 @@
+from pip._vendor.distlib.compat import raw_input
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    student_marks = {}
+    for _ in range(n):
+        line = raw_input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = raw_input()
+
+    print("{:.2f}".format(sum(student_marks.get(query_name))/3))
+
+
+
