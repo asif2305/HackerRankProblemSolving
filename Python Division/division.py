8f14f1d181647e9282679307933db0179f9d643b
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-11-25 12:45:42 +0100

    python-division problem

---
+++ Python Division/division.py
@@ -0,0 +1,11 @@
+from __future__ import division
+
+from pip._vendor.distlib.compat import raw_input
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+    print(a//b)
+    print(a/b)
+
