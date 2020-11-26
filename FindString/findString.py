722834191e8a00cc28a58de3cf67d6445659be8d
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-11-26 19:25:31 +0100

    solved find a substring from a string

---
+++ FindString/findString.py
@@ -0,0 +1,15 @@
+def count_substring(string, sub_string):
+    cnt=0
+    sub_len=len(sub_string)
+    for i in range(len(string)):
+        if string[i:i+sub_len]==sub_string:
+            cnt+=1
+    return cnt
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
